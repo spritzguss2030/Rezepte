1acc0b6b4006e376423a523cceab2717b2d5e760
spritzguss2030 <patrick.schweiger01@gmail.com>
2026-04-12 10:12:01 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,43 +1,10 @@
-const CACHE_NAME = 'reelrezepte-v1';
-const SHELL_ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL_ASSETS))
-  );
-  self.skipWaiting();
-});
-
+// Service Worker deaktiviert
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  if (url.pathname.startsWith('/api/') || url.pathname.startsWith('/.netlify/')) {
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        if (response.ok && event.request.method === 'GET') {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        }
-        return response;
-      });
-    })
-  );
 });
